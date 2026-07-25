845305029abe304f862a76817305d7733f9882ce
lindokuhlesithole <lindokuhlesithole899@gmail.com>
2026-07-25 18:55:02 +0200

    Fix main.tsx: use getRouter() instead of router export

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import { getRouter } from "./router";
 import "./styles.css";
 
-// Create the router instance
+const router = getRouter();
+
 const rootElement = document.getElementById("root");
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
